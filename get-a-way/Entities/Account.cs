693e8a0268ef5bbfd11a9390676f96b172a9aed1
sofeeka <sofiiahrychukh@gmail.com>
2024-10-24 00:27:07 +0200

    Attemped adding class extent

--- get-a-way/Entities/Account.cs
+++ get-a-way/Entities/Account.cs
@@ -1,10 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using get_a_way.Exceptions;
+using get_a_way.Services;
 
 namespace get_a_way.Entities;
 
-abstract class Account
+abstract class Account : IExtent<Account>
 {
+    private static List<Account> extent = new List<Account>();
+    
+    // todo check values for exceptions, make visible public fields (look class diagram implementation p1 in assignment)
+    
     [Key]
     [Required]
     private long ID { get; set; }
@@ -18,7 +24,7 @@ abstract class Account
     [Required]
     [EmailAddress]
     private string Email { get; set; }
-    private string? ProfilePictureUrl { get; set; } // todo figure out how to store images
+    private string? ProfilePictureUrl { get; set; } // todo figure out how to store images, add a default picture
     private bool Verified { get; set; }
     
     [Range(0.0, 10.0)]
@@ -27,4 +33,36 @@ abstract class Account
 
     [ForeignKey("Account")] 
     private List<Account> Followings { get; set; }
+    
+    protected Account(long id, string username, string password, string email)
+    {
+        ID = id;
+        Username = username;
+        Password = password;
+        Email = email;
+        Verified = false;
+        Rating = 10.0;
+        Languages = new List<string>();
+        Followings = new List<Account>();
+
+        extent.Add(this);
+    }
+
+    public List<Account> GetExtentUnmodifiable()
+    {
+        // copy of the list to avoid unintentional changes
+        return new List<Account>(extent);
+    }
+
+    public void AddInstanceToExtent(Account instance)
+    {
+        if (instance == null)
+            throw new AddingNullInstanceException();
+        extent.Add((instance));
+    }
+
+    public void RemoveInstanceFromExtent(Account instance)
+    {
+        extent.Remove(instance);
+    }
 }
