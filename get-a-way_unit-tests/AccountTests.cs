ffb73e3f9dc7da7e740e890c42fe71e4857376da
s28299 <s28299@pjwstk.edu.pl>
2024-10-24 22:25:30 +0200

    Add unit tests project files

---
+++ get-a-way_unit-tests/AccountTests.cs
@@ -0,0 +1,15 @@
+namespace get_a_way_unit_tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
