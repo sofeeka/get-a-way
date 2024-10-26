7b0ab2848c48d4a4f26281795286ed9dbda1d974
marichka <s28299@pjwstk.edu.pl>
2024-10-26 02:06:57 +0200

    Create Amenity.cs

---
+++ get-a-way/Entities/Places/Accommodation/Amenity.cs
@@ -0,0 +1,39 @@
+namespace get_a_way.Entities.Places.Accommodation;
+
+public enum Amenity
+{
+    WiFi,
+    AirConditioning,
+    Heating,
+    TV,
+    Kitchen,
+    Washer,
+    Dryer,
+    Iron,
+    HairDryer,
+    Essentials, // towels, bed sheets...
+    HotWater,
+    Crib,
+    HighChair,
+    SmokeAlarm,
+    CarbonMonoxideAlarm,
+    FirstAidKit,
+    FireExtinguisher,
+    Parking,
+    WheelchairAccessible,
+    PatioOrBalcony,
+    GardenOrBackyard,
+    BBQGrill,
+    Pool,
+    HotTub,
+    Refrigerator,
+    Dishwasher,
+    Stove,
+    CoffeeMaker,
+    Microwave,
+    CookingBasics, 
+    DishesAndSilverware,
+    DedicatedWorkspace, 
+    SelfCheckIn,
+    LuggageDropOffAllowed
+}
