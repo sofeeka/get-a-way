bbeb1802816b9e59383403caadccace5567dae3e
marichka <s28299@pjwstk.edu.pl>
2024-10-26 02:08:06 +0200

    Create DietaryOptions.cs

---
+++ get-a-way/Entities/Places/Eatery/DietaryOptions.cs
@@ -0,0 +1,20 @@
+namespace get_a_way.Entities.Places.Eatery;
+
+public enum DietaryOptions
+{
+    Vegetarian,
+    Vegan,
+    GlutenFree,
+    DairyFree,
+    Halal,
+    Kosher,
+    NutFree,
+    Paleo,
+    Keto,
+    Pescatarian,
+    Organic,
+    LowSodium,
+    LowCarb,
+    SugarFree,
+    Other
+}
