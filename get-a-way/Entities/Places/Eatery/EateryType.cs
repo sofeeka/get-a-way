1c9a4a924d4e7310854656345b0ad3ad8772da8f
marichka <s28299@pjwstk.edu.pl>
2024-10-26 02:07:22 +0200

    Create EateryType.cs

---
+++ get-a-way/Entities/Places/Eatery/EateryType.cs
@@ -0,0 +1,20 @@
+namespace get_a_way.Entities.Places.Eatery;
+
+public enum EateryType
+{
+    Restaurant,
+    Cafe,
+    Bar,
+    Pub,
+    FoodTruck,
+    Bakery,
+    CoffeeShop,
+    Pizzeria,
+    Buffet,
+    Steakhouse,
+    Bistro,
+    DessertShop,
+    SushiBar,
+    WineBar,
+    Other
+}
