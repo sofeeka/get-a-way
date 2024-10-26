108b5c04acec0205b06be1b948a9d4d518c6aa4d
marichka <s28299@pjwstk.edu.pl>
2024-10-26 02:07:44 +0200

    Create Cuisine.cs

---
+++ get-a-way/Entities/Places/Eatery/Cuisine.cs
@@ -0,0 +1,21 @@
+namespace get_a_way.Entities.Places.Eatery;
+
+public enum Cuisine
+{
+    American,
+    Italian,
+    Chinese,
+    Japanese,
+    Mexican,
+    Indian,
+    French,
+    Mediterranean,
+    Thai,
+    Spanish,
+    Greek,
+    MiddleEastern,
+    Vietnamese,
+    Korean,
+    Ukrainian,
+    Other
+}
