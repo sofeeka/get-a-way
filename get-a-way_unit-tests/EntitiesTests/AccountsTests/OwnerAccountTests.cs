fed5c82e6766c81308e61822bf5788c6626e8e88
marichka <s28299@pjwstk.edu.pl>
2024-11-07 14:31:59 +0100

    Owner attributes unit tests

--- get-a-way_unit-tests/EntitiesTests/AccountsTests/OwnerAccountTests.cs
+++ get-a-way_unit-tests/EntitiesTests/AccountsTests/OwnerAccountTests.cs
@@ -1,9 +1,21 @@
 using get_a_way.Entities.Accounts;
+using get_a_way.Exceptions;
 
 namespace get_a_way_unit_tests.EntitiesTests.AccountsTests;
 
 public class OwnerAccountTests
 {
-    
+    [Test]
+    public void SetTax_ValidValue_SetsTax()
+    {
+        OwnerAccount.Tax = 20;
+        Assert.That(OwnerAccount.Tax, Is.EqualTo(20));
+    }
 
+    [Test]
+    public void SetTax_InvalidValue_ThrowsInvalidAttributeException()
+    {
+        Assert.That(() => OwnerAccount.Tax = -5, Throws.TypeOf<InvalidAttributeException>());
+        Assert.That(() => OwnerAccount.Tax = 105, Throws.TypeOf<InvalidAttributeException>());
+    }
 }
