9411c85feaf7f4469e4195f022a19b6b5b803861
sofeeka <sofiiahrychukh@gmail.com>
2024-11-14 03:17:53 +0100

    Update TripTests.cs

--- get-a-way_unit-tests/EntitiesTests/TripTests/TripTests.cs
+++ get-a-way_unit-tests/EntitiesTests/TripTests/TripTests.cs
@@ -7,28 +7,27 @@ public class TripTests
 {
     private Trip _valid;
 
-    private static DateTime _validDate = new DateTime().AddDays(-4);
+    private Account _validAccount = new OwnerAccount("ValidName", "Password123", "valid.email@pjwstk.edu.pl");
+    private static DateTime _now = DateTime.Now;
+    private static DateTime _validDate = new DateTime(_now.Year - 1, _now.Month, _now.Day);
 
     [SetUp]
     public void SetUpEnvironment()
     {
+        Trip.ResetExtent();
         Account.ResetExtent();
-        _valid = new Trip(new OwnerAccount("ValidName", "Password123", "valid.email@pjwstk.edu.pl"),
-            new DateTime().AddDays(-4), TripType.Friends, "Some description");
+        _valid = new Trip(_validAccount, _validDate, TripType.Friends, "Some description");
     }
 
     [Test]
     public void Constructor_ValidAttributes_AssignsCorrectValues()
     {
-        var date = new DateTime().AddDays(-4);
-
-        var trip = new Trip(new OwnerAccount("ValidName", "Password123", "valid.email@pjwstk.edu.pl"),
-            date, TripType.Friends, "Some description");
+        var trip = new Trip(_validAccount, _validDate, TripType.Friends, "Some description");
 
         // ID is 2 because _valid.ID == 1
         Assert.That(trip.ID, Is.EqualTo(2));
 
-        Assert.That(trip.Date, Is.EqualTo(date));
+        Assert.That(trip.Date, Is.EqualTo(_validDate));
         Assert.That(trip.TripType, Is.EqualTo(TripType.Friends));
         Assert.That(trip.Pictures, Is.Empty);
         Assert.That(trip.Description, Is.EqualTo("Some description"));
@@ -37,11 +36,8 @@ public class TripTests
     [Test]
     public void Constructor_NewInstanceCreation_IncrementsId()
     {
-        var test1 = new Trip(new OwnerAccount("ValidName", "Password123", "valid.email@pjwstk.edu.pl"),
-            new DateTime().AddDays(-4), TripType.Friends, "Some description");
-
-        var test2 = new Trip(new OwnerAccount("ValidName", "Password123", "valid.email@pjwstk.edu.pl"),
-            new DateTime().AddDays(-4), TripType.Friends, "Some description");
+        var test1 = new Trip(_validAccount, _validDate, TripType.Friends, "Some description");
+        var test2 = new Trip(_validAccount, _validDate, TripType.Friends, "Some description");
 
         Assert.That(test2.ID - test1.ID, Is.EqualTo(1));
     }
