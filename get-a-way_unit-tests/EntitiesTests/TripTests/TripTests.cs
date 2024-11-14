80921202588ec154c0226cfe17846c32586ee621
sofeeka <sofiiahrychukh@gmail.com>
2024-11-14 03:26:38 +0100

    Update TripTests.cs

--- get-a-way_unit-tests/EntitiesTests/TripTests/TripTests.cs
+++ get-a-way_unit-tests/EntitiesTests/TripTests/TripTests.cs
@@ -1,33 +1,36 @@
 using get_a_way.Entities.Accounts;
 using get_a_way.Entities.Trip;
+using get_a_way.Exceptions;
 
 namespace get_a_way_unit_tests.EntitiesTests.TripTests;
 
 public class TripTests
 {
-    private Trip _valid;
+    private static Trip _valid;
 
-    private Account _validAccount = new OwnerAccount("ValidName", "Password123", "valid.email@pjwstk.edu.pl");
-    private static DateTime _now = DateTime.Now;
-    private static DateTime _validDate = new DateTime(_now.Year - 1, _now.Month, _now.Day);
+    private static readonly Account ValidAccount =
+        new OwnerAccount("ValidName", "Password123", "valid.email@pjwstk.edu.pl");
+
+    private static readonly DateTime Now = DateTime.Now;
+    private static readonly DateTime ValidDate = new DateTime(Now.Year - 1, Now.Month, Now.Day);
 
     [SetUp]
     public void SetUpEnvironment()
     {
         Trip.ResetExtent();
         Account.ResetExtent();
-        _valid = new Trip(_validAccount, _validDate, TripType.Friends, "Some description");
+        _valid = new Trip(ValidAccount, ValidDate, TripType.Friends, "Some description");
     }
 
     [Test]
     public void Constructor_ValidAttributes_AssignsCorrectValues()
     {
-        var trip = new Trip(_validAccount, _validDate, TripType.Friends, "Some description");
+        var trip = new Trip(ValidAccount, ValidDate, TripType.Friends, "Some description");
 
         // ID is 2 because _valid.ID == 1
         Assert.That(trip.ID, Is.EqualTo(2));
 
-        Assert.That(trip.Date, Is.EqualTo(_validDate));
+        Assert.That(trip.Date, Is.EqualTo(ValidDate));
         Assert.That(trip.TripType, Is.EqualTo(TripType.Friends));
         Assert.That(trip.Pictures, Is.Empty);
         Assert.That(trip.Description, Is.EqualTo("Some description"));
@@ -36,9 +39,25 @@ public class TripTests
     [Test]
     public void Constructor_NewInstanceCreation_IncrementsId()
     {
-        var test1 = new Trip(_validAccount, _validDate, TripType.Friends, "Some description");
-        var test2 = new Trip(_validAccount, _validDate, TripType.Friends, "Some description");
+        var test1 = new Trip(ValidAccount, ValidDate, TripType.Friends, "Some description");
+        var test2 = new Trip(ValidAccount, ValidDate, TripType.Friends, "Some description");
 
         Assert.That(test2.ID - test1.ID, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Setter_ValidDate_SetsDate()
+    {
+        var anotherValidDate = new DateTime(Now.Year - 2, Now.Month, Now.Day);
+        _valid.Date = anotherValidDate;
+        Assert.That(_valid.Date, Is.EqualTo(anotherValidDate));
+    }
+
+    [Test]
+    public void Setter_InvalidDate_ThrowsInvalidAttributeException()
+    {
+        Assert.That(() => _valid.Date = new DateTime(Now.Year + 1, Now.Month, Now.Day),
+            Throws.TypeOf<InvalidAttributeException>());
+        Assert.That(() => _valid.Date, Is.EqualTo(ValidDate));
     }
 }
