33a2dc2a0d9432a4482210fb7473cef18d2dd71d
sofeeka <sofiiahrychukh@gmail.com>
2024-12-08 17:05:32 +0100

    Update HistoricalAttraction.cs

--- get-a-way/Entities/Places/Attractions/HistoricalAttraction.cs
+++ get-a-way/Entities/Places/Attractions/HistoricalAttraction.cs
@@ -17,7 +17,10 @@ public class HistoricalAttraction : Attraction
     {
     }
 
-    public HistoricalAttraction(string culturalPeriod)
+    public HistoricalAttraction(string name, string location, DateTime openTime, DateTime closeTime,
+        PriceCategory priceCategory, bool petFriendly, int entryFee, int minimalAge, string description,
+        string culturalPeriod) : base(name, location, openTime, closeTime, priceCategory, petFriendly, entryFee,
+        minimalAge, description)
     {
         CulturalPeriod = culturalPeriod;
     }
@@ -26,6 +29,9 @@ public class HistoricalAttraction : Attraction
     {
         if (string.IsNullOrWhiteSpace(value))
             throw new InvalidAttributeException("Cultural period of historical attraction cannot be empty");
+
+        if (value.Length is < 5 or > 1000)
+            throw new InvalidAttributeException("Cultural period length must be between 5 and 1000 characters.");
 
         return value;
     }
